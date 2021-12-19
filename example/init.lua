60e261f6fab39d45dc9e3f9cb49f7e6f3a3cce93
David Minnerly <voxeldavid@gmail.com>
2021-12-19 13:21:15 -0800

    Add an example

---
+++ example/init.lua
@@ -0,0 +1,18 @@
+local Players = game:GetService("Players")
+
+local example = {}
+
+-- Exposing the Players service allows it to be mocked
+example.Players = Players
+
+function example:getCharacters()
+	local characters = {}
+	for _, player in ipairs(example.Players:GetPlayers()) do
+		if player.Character then
+			table.insert(characters, player.Character)
+		end
+	end
+	return characters
+end
+
+return example
---
+++ example/init.spec.lua
@@ -0,0 +1,28 @@
+return function()
+	local Mock = require(game.ReplicatedStorage.DevPackages.Mock)
+	local example = require(script.Parent.example)
+
+	it("should get all the characters in the experience", function()
+		local mockPlayers = Mock.new()
+
+		-- Create a mock to represent a Player
+		local mockPlayer = Mock.new()
+		mockPlayer.Character = Instance.new("Model")
+
+		-- Define the implementation of Players:GetPlayers()
+		mockPlayers.GetPlayers:mockImplementation(function()
+			return {
+				mockPlayer,
+			}
+		end)
+
+		-- Stub out the actual Players service with a mock
+		example.Players = mockPlayers
+
+		-- Our mocks are setup, so now we can test the getCharacters() function
+		local characters = example:getCharacters()
+
+		expect(#characters).to.equal(1)
+		expect(characters[1]).to.equal(mockPlayer.Character)
+	end)
+end
